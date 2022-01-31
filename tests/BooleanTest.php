09540b2d6d1565c0ece22478c45f0b29f04f021d
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 02:21:26 +0300

    boolean() method with $truePercentage=100 returns always true

--- tests/BooleanTest.php
+++ tests/BooleanTest.php
@@ -5,3 +5,7 @@ declare(strict_types=1);
 test('boolean() method returns a boolean value')
     ->expect(🙃()->number->boolean())
     ->toBeBool();
+
+test('boolean() method with $truePercentage=100 returns always true')
+    ->expect(🙃()->number->boolean(100))
+    ->toBeTrue();
