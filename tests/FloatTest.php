8fbc773b0f92c7809d1be9e8749eec130ba81864
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 02:09:16 +0300

    float() method left digit can be strictly set

--- tests/FloatTest.php
+++ tests/FloatTest.php
@@ -5,3 +5,10 @@ test('float() method returns a float value', function () {
 
     expect($value)->toBeFloat();
 });
+
+test('float() method left digit can be strictly set', function () {
+    $leftDigits = random_int(1, 10);
+    $value = 🙃()->number->float($leftDigits, 0, true);
+
+    expect(strlen($value))->toBe($leftDigits);
+});
