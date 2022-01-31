424563e6fff6ccb62078eff6746a5aef3b78cf68
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 00:49:57 +0300

    digit() method returns a digit

--- tests/DigitTest.php
+++ tests/DigitTest.php
@@ -7,3 +7,10 @@ test('digit() method returns an integer value', function () {
 
     expect($value)->toBeInt();
 });
+
+test('digit() method returns a digit', function () {
+    $value = 🙃()->number->digit();
+
+    expect($value)->toBeGreaterThanOrEqual(0);
+    expect($value)->toBeLessThanOrEqual(9);
+});
