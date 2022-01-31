f5df8d30f7d807e53023afb1039589fb3e1329d6
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 01:55:48 +0300

    integerExcept() method returns an integer except the given integer

--- tests/IntegerExceptTest.php
+++ tests/IntegerExceptTest.php
@@ -7,3 +7,10 @@ test('integerExcept() method returns an integer value', function () {
 
     expect($value)->toBeInt();
 });
+
+test('integerExcept() method returns an integer except the given integer', function () {
+    $value = 🙃()->number->integerExcept(2, 1, 2);
+
+    expect($value)->toBe(1);
+});
+
