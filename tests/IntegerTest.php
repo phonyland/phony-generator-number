f62a1e1d40c49bc55c6a2d7e0bfc45e8adaa8137
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 01:38:37 +0300

    integer() method returns an integer with exactly the given number of $digits

--- tests/IntegerTest.php
+++ tests/IntegerTest.php
@@ -14,3 +14,10 @@ test('integer() method returns an integer with the given number of $digits', fun
 
     expect(strlen((string) abs($value)))->toBeLessThanOrEqual($numberOfDigits);
 });
+
+test('integer() method returns an integer with exactly the given number of $digits', function () {
+    $numberOfDigits = random_int(1, 15);
+    $value = 🙃()->number->integer($numberOfDigits, true);
+
+    expect(strlen((string) abs($value)))->toBeLessThanOrEqual($numberOfDigits);
+});
