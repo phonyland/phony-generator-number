0e64cf4727f5d0a308ebd615a28792e502d395c5
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 01:48:21 +0300

    integerWeighted() method calculates integers with standard deviation

--- tests/IntegerWeightedTest.php
+++ tests/IntegerWeightedTest.php
@@ -7,3 +7,27 @@ test('integerWeighted() method returns an integer value', function () {
 
     expect($value)->toBeInt();
 });
+
+test('integerWeighted() method calculates integers with standard deviation', function () {
+    $n = 10000;
+
+    $values = [];
+    foreach (range(1, 10000) as $k => $i) {
+        $values[] = 🙃()->number->integerNormal(150, 100);
+    }
+
+    $mean = array_sum($values) / (float) $n;
+
+    $variance = array_reduce(
+        $values,
+        static function ($variance, $item) use ($mean) {
+                return $variance + ($item - $mean) ** 2;
+            },
+        0
+    ) / (float) ($n - 1);
+
+    $std_dev = sqrt($variance);
+
+    expect($mean)->toEqualWithDelta(150, 5);
+    expect($std_dev)->toEqualWithDelta(100, 3);
+});
