e029477eec6f4ca29d26117c3cb5d15f86ca98ba
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 02:02:45 +0300

    floatBetween() method returns a float between $min and $max

--- tests/FloatBetweenTest.php
+++ tests/FloatBetweenTest.php
@@ -7,3 +7,11 @@ test('floatBetween() method returns a float value', function () {
 
     expect($value)->toBeFloat();
 });
+
+test('floatBetween() method returns a float between $min and $max', function () {
+    $value = 🙃()->number->floatBetween(0.0, 1.0);
+
+    expect($value)->toBeGreaterThanOrEqual(0);
+    expect($value)->toBeLessThanOrEqual(1);
+});
+
