4ad07df36a454172621622f0d49ba961f9ceb26a
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 02:10:57 +0300

    floatNormal() method calculates floats with standard deviation

--- tests/FloatWeightedTest.php
+++ tests/FloatWeightedTest.php
@@ -7,3 +7,24 @@ test('floatNormal() method returns a float', function () {
 
     expect($value)->toBeFloat();
 });
+
+test('floatNormal() method calculates floats with standard deviation', function () {
+    $n = 10000;
+
+    $values = [];
+    foreach (range(1, 10000) as $k => $i) {
+        $values[] = 🙃()->number->floatNormal(150.0, 100.0);
+    }
+
+    $mean = array_sum($values) / (float) $n;
+
+    $variance = array_reduce(
+            $values, static fn ($variance, $item) => $variance + ($item - $mean) ** 2, 0
+        ) / (float) ($n - 1);
+
+    $std_dev = sqrt($variance);
+
+    expect($mean)->toEqualWithDelta(150, 5);
+    expect($std_dev)->toEqualWithDelta(100, 3);
+});
+
