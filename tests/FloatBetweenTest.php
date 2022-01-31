ae46dbfb89a6223c1bf5b8ee3c554eddaaaf2fe0
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 02:03:05 +0300

    floatBetween() method returns a float with given $precision

--- tests/FloatBetweenTest.php
+++ tests/FloatBetweenTest.php
@@ -15,3 +15,9 @@ test('floatBetween() method returns a float between $min and $max', function () 
     expect($value)->toBeLessThanOrEqual(1);
 });
 
+test('floatBetween() method returns a float with given $precision', function () {
+    $precision = random_int(0, 14);
+    $value = 🙃()->number->floatBetween(0.0, 1.0, $precision);
+
+    expect(strlen($value))->toBeLessThanOrEqual($precision + 2);
+});
