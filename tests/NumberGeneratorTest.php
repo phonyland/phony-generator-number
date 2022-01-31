e46f873011127a3a5d97d1e8c595e2de1a7bf97d
Yunus Emre Deligöz <ye@deligoz.me>
2022-01-31 22:29:30 +0300

    integerBetween() method returns an integer between $min and $max

--- tests/NumberGeneratorTest.php
+++ tests/NumberGeneratorTest.php
@@ -6,4 +6,9 @@ test('integerBetween() method returns an integer value')
     ->expect(🙃()->number->integerBetween())
     ->toBeInt();
 
+test('integerBetween() method returns an integer between $min and $max')
+    ->expect(🙃()->number->integerBetween(1, 100))
+    ->toBeGreaterThanOrEqual(1)
+    ->toBeLessThanOrEqual(100);
+
 // endregion
