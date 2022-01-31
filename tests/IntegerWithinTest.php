80587f8fd6e496003bf5cb968e7bc70f527c2d6b
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 00:35:16 +0300

    integerWithin() method returns an integer that the boundaries not included

--- tests/IntegerWithinTest.php
+++ tests/IntegerWithinTest.php
@@ -5,3 +5,11 @@ test('integerWithin() method returns an integer value', function () {
 
     expect($value)->toBeInt();
 });
+
+test('integerWithin() method returns an integer that the boundaries not included', function () {
+    $value = 🙃()->number->integerWithin(1, 100);
+
+    expect($value)->toBeGreaterThanOrEqual(2);
+    expect($value)->toBeLessThanOrEqual(99);
+});
+
