5a6c00a30fcd1c4b39034f03ab245fbac60e17a1
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 02:22:23 +0300

    boolean() method with $truePercentage=0 returns always false

--- tests/BooleanTest.php
+++ tests/BooleanTest.php
@@ -9,3 +9,7 @@ test('boolean() method returns a boolean value')
 test('boolean() method with $truePercentage=100 returns always true')
     ->expect(🙃()->number->boolean(100))
     ->toBeTrue();
+
+test('boolean() method with $truePercentage=0 returns always false')
+    ->expect(🙃()->number->boolean(0))
+    ->toBeFalse();
