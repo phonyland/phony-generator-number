0b3f41c0aaaef2ded4f9a3b50e32e301e37edb8b
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-01 00:44:59 +0300

    integerNegative() method returns error if $max=0

--- tests/IntegerNegativeTest.php
+++ tests/IntegerNegativeTest.php
@@ -18,3 +18,6 @@ test('integerNegative() method returns error if $max is not negative', function 
     🙃()->number->integerNegative(1);
 })->throws(Error::class);
 
+test('integerNegative() method returns error if $max=0', function () {
+    🙃()->number->integerNegative(0);
+})->throws(Error::class);
